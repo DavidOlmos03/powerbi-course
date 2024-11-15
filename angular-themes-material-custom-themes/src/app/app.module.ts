0854057afdc229c155ab7244e681729630786fba
Juan David Ruiz Olmos <juanda_201001@hotmail.com>
2024-11-15 15:06:36 -0500

    first commit

---
+++ angular-themes-material-custom-themes/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ThemeSwitcherComponent } from './components/theme-switcher/theme-switcher.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ThemeSwitcherComponent
+  ],
+  imports: [
+    BrowserModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
